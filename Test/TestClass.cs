68eb6070f7967bf56cd4902c2e27997b15e84c9d
artkharchenko <artkharchenko@ozon.ru>
2021-04-18 14:26:24 +0300

    Master test

--- Test/TestClass.cs
+++ Test/TestClass.cs
@@ -8,7 +8,7 @@ namespace Test
         {
             Console.WriteLine("My Test App");
             Console.WriteLine("Third version");
-            //This is master
+            //This is master test
         }
     }
 }
